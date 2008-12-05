557696a735e87eb3110273bc7f732a84c66a697a
sonots <sonots@1f6bbde2-83f9-11dd-a5c9-f1483cedc3eb>
2008-12-05 01:40:56 +0000

    seperate files

--- cvmxmatconv.h
+++ cvmxmatconv.h
@@ -2,6 +2,8 @@
 * OpenCV versus Matlab C Library (MEX) interfaces
 *     verified under OpenCV 1.00 and Matlab 2007b
 *
+* Matrix conversion
+*
 * The MIT License
 * 
 * Copyright (c) 2008, Naotoshi Seo <sonots(at)sonots.com>
@@ -24,8 +26,8 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 * THE SOFTWARE.
 */
-#ifndef CV_MEX_INCLUDED
-#define CV_MEX_INCLUDED
+#ifndef CVMX_MATCONV_INCLUDED
+#define CVMX_MATCONV_INCLUDED
 
 #ifdef _MSC_VER
 #pragma warning(push)
@@ -42,197 +44,16 @@
 #include "cv.h"
 #include "mat.h"
 #include "matrix.h"
+#include "cvmxtypeconv.h"
 
-/**
-* definitions 
-*/
-CVAPI(int)          cvmxIplToCvDepth(int depth);
-CVAPI(mxClassID)    cvmxClassIDFromIplDepth(int depth);
-CV_INLINE mxClassID cvmxClassIDFromCvDepth(int type);
-CVAPI(int)          cvmxClassIDToIplDepth(mxClassID classid);
-CV_INLINE int       cvmxClassIDToCvDepth(mxClassID classid);
+/************** Definitions *******************************/
 CVAPI(mxArray*)     cvmxArrayFromCvArr(const CvArr* arr);
-CV_INLINE mxArray*  cvmxArrayFromIplImage(const IplImage* img);
-CV_INLINE mxArray*  cvmxArrayFromCvMat(const CvMat* mat);
-CVAPI(CvArr*)       cvmxArrayToCvArr(const mxArray* mxarr);
-CV_INLINE IplImage* cvmxArrayToIplImage(const mxArray* mxarr);
-CV_INLINE CvMat*    cvmxArrayToCvMat(const mxArray* mxarr);
+#define cvmxArrayFromIplImage(img) (cvmxArrayFromCvArr(img))
+#define cvmxArrayFromCvMat(mat) (cvmxArrayFromCvArr(mat))
+CVAPI(IplImage*)    cvmxArrayToIplImage(const mxArray* mxarr);
+CVAPI(CvMat*)       cvmxArrayToCvMat(const mxArray* mxarr);
 
-/**
-* Convert CvMat type to IplImage depth
-*
-* @param int ICvMat type
-* @return int IplImage depth
-* @see cvmxClassIDFromCvDepth
-* @see cvmxClassIDToCvDepth
-* @see cvmxClassIDFromIplDepth
-* @see cvmxClassIDToIplDepth
-* @see cvmxIplToCvDepth
-* @see cvCvToIplDepth
-*/
-#define cvmxCvToIplDepth(type) cvCvToIplDepth(type)
-
-/**
-* Convert IplImage depth to CvMat depth
-*
-* One may use CV_MAKETYPE(depth, nChannel) to create cvMat type
-*
-* @param int IplImage depth
-* @return int cvMat depth
-* @see cvmxClassIDFromCvDepth
-* @see cvmxClassIDToCvDepth
-* @see cvmxClassIDFromIplDepth
-* @see cvmxClassIDToIplDepth
-* @see cvmxIplToCvDepth
-* @see cvCvToIplDepth
-* @reference cxcore/src/_cxcore.h#icvIplToCvDepth
-* @reference ./cxcore/src/cxtables.cpp
-*/
-CVAPI(int) cvmxIplToCvDepth(int depth)
-{
-    /*
-    #define CV_8U   0
-    #define CV_8S   1
-    #define CV_16U  2
-    #define CV_16S  3
-    #define CV_32S  4
-    #define CV_32F  5
-    #define CV_64F  6
-    #define CV_USRTYPE1 7 */
-    /*
-    #define IPL_DEPTH_SIGN 0x80000000
-    #define IPL_DEPTH_1U     1
-    #define IPL_DEPTH_8U     8
-    #define IPL_DEPTH_16U   16
-    #define IPL_DEPTH_32F   32
-    #define IPL_DEPTH_64F   64
-    #define IPL_DEPTH_8S  (IPL_DEPTH_SIGN| 8)
-    #define IPL_DEPTH_16S (IPL_DEPTH_SIGN|16)
-    #define IPL_DEPTH_32S (IPL_DEPTH_SIGN|32) */
-    /*
-    typedef enum {
-    mxUNKNOWN_CLASS,
-    mxCELL_CLASS,
-    mxSTRUCT_CLASS,
-    mxLOGICAL_CLASS,
-    mxCHAR_CLASS,
-    mxDOUBLE_CLASS,
-    mxSINGLE_CLASS,
-    mxINT8_CLASS,
-    mxUINT8_CLASS,
-    mxINT16_CLASS,
-    mxUINT16_CLASS,
-    mxINT32_CLASS,
-    mxUINT32_CLASS,
-    mxINT64_CLASS,
-    mxUINT64_CLASS,
-    mxFUNCTION_CLASS
-    } mxClassID; */
-    static const signed char IplToCvDepth[] =
-    {
-        -1, -1, CV_8U, CV_8S, CV_16U, CV_16S, -1, -1,
-        CV_32F, CV_32S, -1, -1, -1, -1, -1, -1, CV_64F, -1
-    };
-    return IplToCvDepth[(((depth) & 255) >> 2) + ((depth) < 0)];
-}
-
-/**
-* Get mxClassID from IplImage depth
-* 
-* @param int depth IplImage depth
-* @return mxClassID
-* @see cvmxClassIDFromCvDepth
-* @see cvmxClassIDToCvDepth
-* @see cvmxClassIDFromIplDepth
-* @see cvmxClassIDToIplDepth
-* @see cvmxIplToCvDepth
-* @see cvCvToIplDepth
-*/
-CVAPI(mxClassID) cvmxClassIDFromIplDepth(int depth)
-{
-    static const unsigned char ClassIDFromIplDepth[] =
-    {
-        0, 0, mxUINT8_CLASS, mxINT8_CLASS, mxUINT16_CLASS, mxINT16_CLASS, 0, 0,
-        mxSINGLE_CLASS, mxINT32_CLASS, 0, 0, 0, 0, 0, 0, mxDOUBLE_CLASS, 0
-    };
-    if (depth == IPL_DEPTH_1U) {
-        return mxLOGICAL_CLASS;
-    } else {
-        return (mxClassID)ClassIDFromIplDepth[(((depth) & 255) >> 2) + ((depth) < 0)];
-    }
-}
-
-/**
-* Convert mxClassID to IplImage depth 
-* 
-* @param int depth IplImage's depth
-* @return mxClassID
-* @see cvmxClassIDFromCvDepth
-* @see cvmxClassIDToCvDepth
-* @see cvmxClassIDFromIplDepth
-* @see cvmxClassIDToIplDepth
-* @see cvmxIplToCvDepth
-* @see cvCvToIplDepth
-*/
-CVAPI(int) cvmxClassIDToIplDepth(mxClassID classid)
-{
-    static const signed int ClassIDToIplDepth[] = {
-        0,
-        0,
-        0,
-        IPL_DEPTH_1U,
-        0,
-        IPL_DEPTH_32F,
-        IPL_DEPTH_64F,
-        IPL_DEPTH_8S,
-        IPL_DEPTH_8U,
-        IPL_DEPTH_16S,
-        IPL_DEPTH_16U,
-        IPL_DEPTH_32S,
-        0,
-        0,
-        0,
-        0
-    };
-    return ClassIDToIplDepth[classid];
-}
-
-/**
-* Get mxClassID from cvMat type
-*
-* @param int cvMat type
-* @return mxClassID
-* @see cvmxClassIDFromCvDepth
-* @see cvmxClassIDToCvDepth
-* @see cvmxClassIDFromIplDepth
-* @see cvmxClassIDToIplDepth
-* @see cvmxIplToCvDepth
-* @see cvCvToIplDepth
-*/
-CV_INLINE mxClassID cvmxClassIDFromCvDepth(int type)
-{
-    return cvmxClassIDFromIplDepth(cvCvToIplDepth(type));
-}
-
-/**
-* Convert mxClassID to cvMat depth
-*
-* One may use CV_MAKETYPE(depth, nChannel) to create cvMat type
-*
-* @param mxClassID classid
-* @return int cvMat depth
-* @see cvmxClassIDFromCvDepth
-* @see cvmxClassIDToCvDepth
-* @see cvmxClassIDFromIplDepth
-* @see cvmxClassIDToIplDepth
-* @see cvmxIplToCvDepth
-* @see cvCvToIplDepth
-*/
-CV_INLINE int cvmxClassIDToCvDepth(mxClassID classid)
-{
-    return cvmxIplToCvDepth(cvmxClassIDToIplDepth(classid));
-}
-
+/************** Functions *********************************/
 /**
 * Convert CvArr to mxArray. Allocate memory too
 *
@@ -300,37 +121,15 @@ CVAPI(mxArray*) cvmxArrayFromCvArr(const CvArr* arr)
 }
 
 /**
-* Convert mxArray from IplImage. Allocate memory too
-*
-* @param IplImage*
-* @return mxArray*
-*/
-CV_INLINE mxArray* cvmxArrayFromIplImage(const IplImage* img)
-{
-    return cvmxArrayFromCvArr(img);
-}
-
-/**
-* Convert mxArray from CvMat. Allocate memory too
-*
-* @param CvMat*
-* @return mxArray*
-*/
-CV_INLINE mxArray* cvmxArrayFromCvMat(const CvMat* mat)
-{
-    return cvmxArrayFromCvMat(mat);
-}
-
-/**
-* Convert mxArray to CvArr. Allocate memory too
+* Convert mxArray to IplImage. Allocate memory too
 *
 * Currently support only uint8, float, double
 * Currently support only 1 channel (grayscale) and 3 channels (rgb etc)
 *
 * @param mxArray* mxarr
-* @return CvArr*
+* @return IplImage*
 */
-CVAPI(CvArr*) cvmxArrayToCvArr(const mxArray* mxarr)
+CVAPI(IplImage*) cvmxArrayToIplImage(const mxArray* mxarr)
 {
     CV_FUNCNAME( "cvmxArrayToCvArr" );
     IplImage *img = NULL;
@@ -381,21 +180,7 @@ CVAPI(CvArr*) cvmxArrayToCvArr(const mxArray* mxarr)
         }
     }
     __END__;
-    return (CvArr*)img;
-}
-
-/**
-* Convert mxArray to IplImage. Allocate memory too
-*
-* Currently support only uint8, float, double
-* Currently support only 1 channel (grayscale) and 3 channels (rgb etc)
-*
-* @param mxArray* mxarr
-* @return IplImage*
-*/
-CV_INLINE IplImage* cvmxArrayToIplImage(const mxArray* mxarr)
-{
-    return (IplImage*)cvmxArrayToCvArr(mxarr);
+    return img;
 }
 
 /**
@@ -407,7 +192,7 @@ CV_INLINE IplImage* cvmxArrayToIplImage(const mxArray* mxarr)
 * @param mxArray* mxarr
 * @return CvMat*
 */
-CV_INLINE CvMat* cvmxArrayToCvMat(const mxArray* mxarr)
+CVAPI(CvMat*) cvmxArrayToCvMat(const mxArray* mxarr)
 {
     IplImage* img = cvmxArrayToIplImage(mxarr);
     CvMat *mat = cvCreateMat(img->height, img->width, CV_MAKETYPE(cvmxIplToCvDepth(img->depth), img->nChannels));
@@ -415,11 +200,9 @@ CV_INLINE CvMat* cvmxArrayToCvMat(const mxArray* mxarr)
     cvReleaseImage(&img);
     // @todo: why cvGetMat results in error?
     //int coi = 0;
-    //CvMat mathdr, *mat = (CvMat*)cvmxArrayToCvArr(mxarr);
-    //if( !CV_IS_MAT(mat) )
-    //{
-    //    mat = cvGetMat(mat, &mathdr, &coi, 0);
-    //}
+    //IplImage *img = cvmxArrayToIplImage(mxarr);
+    //CvMat *mat, mathdr;
+    //mat = cvGetMat(mat, &mathdr, &coi, 0);
     return mat;
 }
 
