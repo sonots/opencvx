0a129bd8a7572a6689869755bfcefe94cdc5e803
sonots <sonots@1f6bbde2-83f9-11dd-a5c9-f1483cedc3eb>
2008-12-05 16:19:23 +0000

    (new) Rotated Rectangle states + 2nd AR dynamics model for particle filter

--- cvparticle.h
+++ cvparticle.h
@@ -1,26 +1,34 @@
 /** @file
-* The MIT License
-* 
-* Copyright (c) 2008, Naotoshi Seo <sonots(at)sonots.com>
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
+ * Particle Filter
+ *
+ * Currently suppports only linear state transition model. 
+ * Write up a function by yourself to supports nonlinear dynamics
+ * such as Taylor series model and call your function instead of 
+ * cvParticleTransition( p ). 
+ * Other functions should not necessary be modified.
+ *
+ * The MIT License
+ * 
+ * Copyright (c) 2008, Naotoshi Seo <sonots(at)sonots.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #ifndef CV_PARTICLE_INCLUDED
 #define CV_PARTICLE_INCLUDED
 
@@ -259,10 +267,9 @@ void cvParticleBound( CvParticle* p )
 /**
  * Samples new particles from given particles
  *
- * Currently suppports only linear dynamics and linear combinations.
- * Write up a function by yourself to supports nonlinear dynamics or
- * nonlinear combinations and call your function instead of
- * cvParticleTrantion( p );
+ * Currently suppports only linear combination of states transition model. 
+ * Write up a function by yourself to supports nonlinear dynamics
+ * such as Taylor series model and call your function instead of this function. 
  * Other functions should not necessary be modified.
  *
  * @param particle
