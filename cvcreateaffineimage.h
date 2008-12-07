57fae14a9cb959e4e766f44550d70ed0e013d78e
sonots <sonots@1f6bbde2-83f9-11dd-a5c9-f1483cedc3eb>
2008-12-07 04:42:41 +0000

    (bug)

--- cvcreateaffineimage.h
+++ cvcreateaffineimage.h
@@ -21,8 +21,8 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 * THE SOFTWARE.
 */
-#ifndef CV_AFFINEIMAGE_INCLUDED
-#define CV_AFFINEIMAGE_INCLUDED
+#ifndef CV_CREATEAFFINEIMAGE_INCLUDED
+#define CV_CREATEAFFINEIMAGE_INCLUDED
 
 #include "cv.h"
 #include "cvaux.h"
@@ -87,29 +87,33 @@ CVAPI(IplImage*) cvCreateAffineImage( const IplImage* src, const CvMat* affine,
     int miny = INT_MAX;
     int maxx = INT_MIN;
     int maxy = INT_MIN;
+    int i, x, y, xx, yy, xp, yp;
+    int ch, width, height;
+    CvPoint pt[4];
+    CvMat* invaffine;
     CV_FUNCNAME( "cvAffineImage" );
     __BEGIN__;
     CV_ASSERT( src->depth == IPL_DEPTH_8U );
     CV_ASSERT( affine->rows == 2 && affine->cols == 3 );
 
-    CvPoint pt[4];
     // original 4 corner
     pt[0].x = 0;              pt[0].y = 0;
     pt[1].x = src->width - 1; pt[1].y = 0;
     pt[2].x = 0;              pt[2].y = src->height - 1;
     pt[3].x = src->width - 1; pt[3].y = src->height - 1;
     // 4 corner after transformed
-    for( int i = 0; i < 4; i++ )
+    for( i = 0; i < 4; i++ )
     {
-        pt[i].x = cvRound( pt[i].x * cvmGet( affine, 0, 0 ) + 
-                           pt[i].y * cvmGet( affine, 0, 1 ) + 
-                           cvmGet( affine, 0, 2 ) );
-        pt[i].y = cvRound( pt[i].x * cvmGet( affine, 1, 0 ) + 
-                           pt[i].y * cvmGet( affine, 1, 1 ) + 
-                           cvmGet( affine, 1, 2 ) );
+        x = cvRound( pt[i].x * cvmGet( affine, 0, 0 ) + 
+                     pt[i].y * cvmGet( affine, 0, 1 ) + 
+                     cvmGet( affine, 0, 2 ) );
+        y = cvRound( pt[i].x * cvmGet( affine, 1, 0 ) + 
+                     pt[i].y * cvmGet( affine, 1, 1 ) + 
+                     cvmGet( affine, 1, 2 ) );
+        pt[i].x = x; pt[i].y = y;
     }
     // min, max
-    for( int i = 0; i < 4; i++ )
+    for( i = 0; i < 4; i++ )
     {
         minx = MIN( pt[i].x, minx );
         miny = MIN( pt[i].y, miny );
@@ -117,7 +121,6 @@ CVAPI(IplImage*) cvCreateAffineImage( const IplImage* src, const CvMat* affine,
         maxy = MAX( pt[i].y, maxy );
     }
     // target image width and height
-    int width, height;
     if( flags == CV_AFFINE_FULL )
     {
         width = maxx - minx + 1;
@@ -131,6 +134,8 @@ CVAPI(IplImage*) cvCreateAffineImage( const IplImage* src, const CvMat* affine,
         maxx = src->width - 1;
         maxy = src->height - 1;
     }
+    //cvPrintMat( affine );
+    //printf( "%d %d %d %d\n", minx, miny, maxx, maxy );
     if( origin != NULL )
     {
         origin->x = minx;
@@ -140,24 +145,24 @@ CVAPI(IplImage*) cvCreateAffineImage( const IplImage* src, const CvMat* affine,
     cvSet( dst, color );
 
     // inverse affine
-    CvMat* invaffine = cvCreateMat( 2, 3, affine->type );
+    invaffine = cvCreateMat( 2, 3, affine->type );
     cvInvAffine( affine, invaffine );
     
     // loop based on image coordinates of transformed image
-    for( int x = 0; x < width; x++ )
+    for( x = 0; x < width; x++ )
     {
-        int xx = x + minx;
-        for( int y = 0; y < height; y++ )
+        xx = x + minx;
+        for( y = 0; y < height; y++ )
         {
-            int yy = y + miny;
-            int xp = cvRound( xx * cvmGet( invaffine, 0, 0 ) + 
+            yy = y + miny;
+            xp = cvRound( xx * cvmGet( invaffine, 0, 0 ) + 
                               yy * cvmGet( invaffine, 0, 1 ) + 
                               cvmGet( invaffine, 0, 2 ) );
-            int yp = cvRound( xx * cvmGet( invaffine, 1, 0 ) + 
+            yp = cvRound( xx * cvmGet( invaffine, 1, 0 ) + 
                               yy * cvmGet( invaffine, 1, 1 ) + 
                               cvmGet( invaffine, 1, 2 ) );
             if( xp < 0 || xp >= src->width || yp < 0 || yp >= src->height ) continue;
-            for( int ch = 0; ch < src->nChannels; ch++ )
+            for( ch = 0; ch < src->nChannels; ch++ )
             {
                 dst->imageData[dst->widthStep * y + x * dst->nChannels + ch]
                     = src->imageData[src->widthStep * yp + xp * src->nChannels + ch];
--- tests/cvcreateaffineimage.cpp
+++ tests/cvcreateaffineimage.cpp
@@ -26,10 +26,10 @@ int main( int argc, char** argv )
     }
     img = cvLoadImage( argv[1] );
     CvMat* affine = cvCreateMat( 2, 3, CV_64FC1 );
-    cvCreateAffine( affine, cvRect32f( 0, 0, 1, 1/2.0, 10 ), cvPoint2D32f( 0, 0 ) );
+    cvCreateAffine( affine, cvRect32f( 0, 0, 1/4.0, 1/4.0, 45 ), cvPoint2D32f( 0, 0 ) );
     IplImage* transformed;
 
-    transformed = cvCreateAffinedImage( img, affine, CV_AFFINE_SAME );
+    transformed = cvCreateAffineImage( img, affine, CV_AFFINE_SAME );
     cvNamedWindow( "hoge" );
     cvShowImage( "hoge", transformed );
     cvWaitKey( 0 );
@@ -46,8 +46,7 @@ int main( int argc, char** argv )
     IplImage *mask = cvCreateAffineMask( img, affine, CV_AFFINE_FULL );
     cvNamedWindow( "hoge" );
     //cvShowImage( "hoge", mask ); // 1 is 1/255...
-    printf( "origin.x = %d origin.y = %d\n", origin.x, origin.y );
-    cvWaitKey( 0 );
+    //cvWaitKey( 0 );
     cvReleaseImage( &mask );
 
     cvReleaseMat( &affine );
