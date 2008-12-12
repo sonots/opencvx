82fce27debf625e28e84fd0d2b101dae35fef64b
sonots <sonots@1f6bbde2-83f9-11dd-a5c9-f1483cedc3eb>
2008-12-12 04:33:26 +0000

    doc

--- cvanglemean.h
+++ cvanglemean.h
@@ -31,22 +31,24 @@
 #include <float.h>
 #include <math.h>
 
-CvScalar cvAngleMean( const CvArr *arr, const CvArr *weight = NULL, double wrap = 360 );
+CvScalar cvAngleMean( const CvArr *arr, const CvArr *weight = NULL, 
+                      double wrap = 360 );
 
 /**
  * cvAngleMean
  *
- * Compute mean of angle elements of an array for each channel independently
- * The fact that 0 degrees and 360 degrees are identical angles, 
- * so that for example 180 degrees is not a sensible mean of 2 degrees and 358 degrees
+ * Compute mean of angle elements of an array (each channel independently). 
+ * There is a fact that 0 degrees and 360 degrees are identical angles, 
+ * so that for example 180 degrees is not a sensible mean of 2 degrees and 
+ * 358 degrees. This function computes means of angles by computing mean 
+ * of cosine and sine and finally taking arc tangent. 
  *
- * @param  arr       array having values. 
- * @param  [weight = NULL]
- *                   weight to compute mean. The deafult is 1/num (uniform)
- *                   The size and number of channels must be same with arr.
- * @param  [wrap = 360]
- *                   The unit of wrapping around.
- *                   The defeault is 360 for angle
+ * @param  arr              array
+ * @param  [weight = NULL]  weight to compute mean. 
+ *                          The deafult is 1/num (uniform)
+ *                          The size must be same with arr. 
+ * @param  [wrap = 360]     The unit of wrapping around.
+ *                          The defeault is 360 as angle.
  */
 CvScalar cvAngleMean( const CvArr *arr, const CvArr *weight, double wrap )
 {
@@ -68,17 +70,23 @@ CvScalar cvAngleMean( const CvArr *arr, const CvArr *weight, double wrap )
         {
             CV_CALL( wmat = cvGetMat( wmat, &wmatstub ) );
         }
-        CV_ASSERT( mat->rows == wmat->rows && 
-                   mat->cols == wmat->cols &&
-                   CV_MAT_CN(mat->type) == CV_MAT_CN(wmat->type) );
+        CV_ASSERT( 
+            mat->rows == wmat->rows && 
+            mat->cols == wmat->cols &&
+            CV_MAT_CN(mat->type) == CV_MAT_CN(wmat->type) 
+        );
     }
-    double w = 1 / (double)mat->rows * mat->cols;
     int row, col, ch;
     int nChannels = CV_MAT_CN(mat->type);
     CvScalar elem;
-    CvScalar welem = cvScalar(w,w,w,w);
     CvScalar mean_cos = cvScalar(0,0,0,0);
     CvScalar mean_sin = cvScalar(0,0,0,0);
+    CvScalar welem;
+    if( wmat == NULL ) // uniform
+    {
+        double w = 1.0 / (double)mat->rows * (double)mat->cols;
+        welem = cvScalar( w, w, w, w );
+    }
     for( row = 0; row < mat->rows; row++ )
     {
         for( col = 0; col < mat->cols; col++ )
@@ -87,14 +95,17 @@ CvScalar cvAngleMean( const CvArr *arr, const CvArr *weight, double wrap )
             if( wmat != NULL ) welem = cvGet2D( wmat, row, col );
             for( ch = 0; ch < nChannels; ch++ )
             {
-                mean_cos.val[ch] += cos( elem.val[ch] * 2*M_PI / wrap ) * welem.val[ch];
-                mean_sin.val[ch] += sin( elem.val[ch] * 2*M_PI / wrap ) * welem.val[ch];
+                mean_cos.val[ch] += 
+                    cos( elem.val[ch] * 2*M_PI / wrap ) * welem.val[ch];
+                mean_sin.val[ch] += 
+                    sin( elem.val[ch] * 2*M_PI / wrap ) * welem.val[ch];
             }
         }
     }
     for( ch = 0; ch < nChannels; ch++ )
     {
-        mean.val[ch] = atan( mean_sin.val[ch] / mean_cos.val[ch] ) * wrap / (2*M_PI);
+        mean.val[ch] = 
+            atan( mean_sin.val[ch] / mean_cos.val[ch] ) * wrap / (2*M_PI);
     }
     __END__;
     return mean;
